d375dec5c09f90efc77c8391c55077fd86561b42
Benjamin Joseph <benjo@sayhello.com>
2014-11-20 09:42:01 -0800

    took out some old comments from pass through classifer

--- AudioFeaturesConsumer/machinelearning/PassThroughClassifier.py
+++ AudioFeaturesConsumer/machinelearning/PassThroughClassifier.py
@@ -29,35 +29,4 @@ class PassThroughClassifier(object):
             
         return None
         
-        
-#message audio_class_data {
-#  enum audio_class {
-#    UNKNOWN = 0;
-#    NULL = 1;
-#    TALKING = 2;
-#    CRYING = 3;
-#    SNORING = 4;
-#    VEHICLE = 5;
-#  }
-#
-#  repeated float probability       = 1; //probability vector
-#  repeated audio_class classes     = 2; //which element in prob vector is what
-#  optional audio_class decision    = 3; //the class on which we decided
-#}
-#
-#message audio_classifcation_message {
-#
-#  repeated float feat_vec                        = 1;//original raw features
-#  optional audio_class_data classification       = 2;//output of classifer
-#  optional int64 time1                           = 3;//segment start time in nanoseconds, referenced from.... boot?
-#  optional int64 time2                           = 4;//segment end time in nanoseconds
-#  optional int32 unix_time                       = 5;//form the client -- wall time UTC, seconds since 1970
-#  optional string classifer_key                  = 6;//which S3 key we used for this classifer
-#  optional string tags                           = 7;//from the client (i.e. Morpheus, or whatever), usually a comma separated list of things like "steady"
-#  optional string source                         = 8;//from the client, which item this was from
-#  optional bytes mac                             = 9;//mac address of client (basically related to user ID)
-#  optional string userid                         = 10; // user id, from our database, redundant with mac address, but may help us a lot with aggregation
-#
-#}
-
 
